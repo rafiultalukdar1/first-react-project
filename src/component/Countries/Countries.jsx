cf6bb6269ef6f5445d48ae2f5116f7bd92429062
rafi0012 <rafiultalukdar007@gmail.com>
2025-09-20 13:12:01 +0600

    Added Functionality

--- src/component/Countries/Countries.jsx
+++ src/component/Countries/Countries.jsx
@@ -1,17 +1,34 @@
-import React, { use } from 'react';
+import React, { use, useState } from 'react';
 import Country from '../Country/Country';
 
 const Countries = ({countriesPromise}) => {
+
+    const [visitedCountries, setVisitedCountries] = useState([]);
+    
+    const handleVisitedCountries = (country) => {
+        const newVisitedCountries = [...visitedCountries, country];
+        setVisitedCountries(newVisitedCountries);
+    };
 
     const countriesData = use(countriesPromise);
     const countries = countriesData.countries;
 
     return (
         <div>
-            <h1 className='py-12 text-[40px] font-[700] text[#141414]'>Countries: {countries.length}</h1>
+            <h1 className='pt-12 text-[40px] font-[700] text[#141414]'>Countries: {countries.length}</h1>
+            <h1 className='pb-5 pt-3 text-[38px] font-[700] text[#141414]'>Total Country Visited: {visitedCountries.length}</h1>
+            <div className='flex flex-wrap gap-[15px] mb-10'>
+                {
+                    visitedCountries.map(country => <div className='border border-[#141414] rounded-[4px] p-[5px] pt-[8px]' key={country.cca3.cca3}>
+                        <img className='max-w-[120px]' src={country.flags.flags.png} alt={country.flags.flags.alt} />
+                        <h2 className='text-[18px] font-[600] text[#141414] pt-[6px] pl-1'>{country.name.common}</h2>
+                        <h2 className='text-[18px] font-[500] text[#141414] pl-1'>{country.capital.capital}</h2>
+                    </div>)
+                }
+            </div>
             <div className='grid lg:grid-cols-3 sm:grid-cols-2 gap-[15px]'>
                 {
-                    countries.map(country => <Country key={country.cca3.cca3} country = {country}></Country>)
+                    countries.map(country => <Country key={country.cca3.cca3} country = {country} handleVisitedCountries= {handleVisitedCountries}></Country>)
                 }
             </div>
         </div>
--- src/component/Country/Country.jsx
+++ src/component/Country/Country.jsx
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Country = ({country}) => {
+const Country = ({country, handleVisitedCountries}) => {
+
+    const [visited, setvisited] = useState(false);
+
+    const handleVisited = () => {
+        setvisited(visited ? false : true)
+        handleVisitedCountries(country);
+    }
     
     return (
-        <div className='border-[2px] border-[#141414] rounded-[8px] px-[12px] py-[12px] bg-[#fff]'>
+        <div className={`border-[2px] border-[#141414] rounded-[8px] px-[12px] py-[12px] ${visited ? 'bg-[#edfceb]' : 'bg-[#fff]'}`}>
             <img className='w-full border rounded-[4px]' src={country.flags.flags.png} alt={country.flags.flags.alt} />
             <h2 className='text-[28px] font-[700] text[#141414] pt-[20px] pl-3'>Name: {country.name.common}</h2>
             <h4 className='text-[22px] font-[600] text[#141414] py-[7px] pl-3'>Population: {country.population.population}</h4>
             <h4 className='text-[22px] font-[600] text[#141414] pl-3'>Area: {country.area.area} <span className='text-[18px] text-[#FF0000]'>{country.area.area > 300000 ? "(Big Country)" : "(Small Country)"}</span></h4>
-            <button className='ml-3 mt-5 px-[20px] py-[10px] rounded-[4px] bg-[#3FBE53] text-[#fff] font-[700] cursor-pointer'>Visit Now</button>
+            <button onClick={handleVisited} className='ml-3 mt-5 px-[20px] py-[10px] rounded-[4px] bg-[#3FBE53] text-[#fff] font-[700] cursor-pointer'>
+                { visited ? "Visited" : "Visit Now" }
+            </button>
         </div>
     );
 };
